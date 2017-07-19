8b8f90821dec877249b4774d53bead4c7d6923b0
randyjap <randyjap@gmail.com>
2017-07-18 22:08:47 -0700

    got authorization code

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, WebView } from 'react-native';
 
 export default class App extends React.Component {
+  // We watch for changes in navigation, because we asked Reddit to redirect us
+  // to an arbitrary URL callback://login when the login has been completed.
+  onNavigationStateChange = (navState) => {
+    if (navState.url.slice(0,22) === 'about://callback/login') {
+      // Regex shortcut to grab the access_token if the URL matches this format.
+      const regex = /^about:\/\/callback\/login\?code=(.+)&state=random_string&uidt=(.+)/
+      let authorization_code = navState.url.match(regex)[1]
+      console.log(authorization_code)
+      // this.props.dispatch(userActionCreators.authenticationSuccess(accessToken))
+    }
+  }
+
   render() {
+    const client_id = 'nprone_trial_mrrw1yBjHbZE'
+    const redirect_uri = 'about%3A%2F%2Fcallback%2Flogin'
+    const scope = 'listening.readonly'
+    const state = 'random_string'
+    const LOGIN_URL = `https://api.npr.org/authorization/v2/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&scope=${scope}&state=${state}`
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+      <WebView
+        source={{uri: LOGIN_URL}}
+        onNavigationStateChange={this.onNavigationStateChange}
+      />
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
